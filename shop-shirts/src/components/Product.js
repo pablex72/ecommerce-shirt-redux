e46f7140a5c235757223c069d4d1770728701183
pablo <tonyp2hoy@gmail.com>
2024-06-30 11:03:45 -0400

    feat: adding cards in fetch/bootstrap

--- shop-shirts/src/components/Product.js
+++ shop-shirts/src/components/Product.js
@@ -8,10 +8,33 @@ const Product = () => {
       .then((data) => data.json())
       .then((result) => setProducts(result));
   }, []);
+
+  const cards = products.map(product =>  (
+      <div className="col-md-3">
+        <div class="card" style={{width: '18rem'}}>
+          <img class="card-img-top" src={product.image} alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  )
+
   return (
     <>
       <h1> Product Dashboard </h1>
-      {JSON.stringify(products)}
+      {/* {JSON.stringify(products)} */}
+      <div className="row">
+        {cards}
+      </div>
     </>
   );
 };
