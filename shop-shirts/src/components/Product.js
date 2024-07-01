007a7ec4b2f5ec952ad2b49cd6a807dbddfd858b
pablo <tonyp2hoy@gmail.com>
2024-07-01 17:57:44 -0400

    feat: restructuring the project

--- shop-shirts/src/components/Product.js
+++ shop-shirts/src/components/Product.js
@@ -9,32 +9,35 @@ const Product = () => {
       .then((result) => setProducts(result));
   }, []);
 
-  const cards = products.map(product =>  (
-      <div className="col-md-3">
-        <div class="card" style={{width: '18rem'}}>
-          <img class="card-img-top" src={product.image} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{marginBottom:'10px'}}>
+      <div class="card" className="h-100" style={{ width: "18rem" }} >
+        <div className="text-center">
+          <img
+            class="card-img-top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+            alt="Card image cap"
+          />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{product.title}</h5>
+          <p class="card-text">
+            {product.price}
+          </p>
+          <a href="#" class="btn btn-primary">
+            Add to Cart
+          </a>
         </div>
       </div>
-    )
-  )
+    </div>
+  ));
 
   return (
     <>
       <h1> Product Dashboard </h1>
       {/* {JSON.stringify(products)} */}
-      <div className="row">
-        {cards}
-      </div>
+      <div className="row">{cards}</div>
     </>
   );
 };
